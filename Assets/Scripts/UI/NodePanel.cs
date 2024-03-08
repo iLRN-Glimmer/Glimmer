95b638f1a312e2825e5c9420e5f560d1c543c9ef
geoffery0 <geoffery@DESKTOP-9U7JLCN.localdomain>
2024-03-08 12:57:32 -0800

    all windows close by clicking outside of them

--- Assets/Scripts/UI/NodePanel.cs
+++ Assets/Scripts/UI/NodePanel.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using UnityEngine.EventSystems;
 using UnityEngine;
+using StarterAssets;
 
 public class NodePanel : MonoBehaviour
 {
@@ -9,6 +11,8 @@ public class NodePanel : MonoBehaviour
     private GameObject NodeDescription;
     private GameObject NodeThumbnail;
     private GameObject NodeQuestion;
+    private GameObject Parent;
+    private List<GameObject> children;
     private string Answer;
 
     // Start is called before the first frame update
@@ -18,13 +22,54 @@ public class NodePanel : MonoBehaviour
         NodeDescription = transform.Find("NodeDescription").gameObject;
         NodeThumbnail = transform.Find("NodeThumbnail").gameObject;
         NodeQuestion = transform.Find("Question/NodeQuestion").gameObject;
+        Parent = transform.parent.gameObject;
+
+        children = new List<GameObject>();
+        AddDescendants(transform, children);
+        children.Add(gameObject);
+        
+    }
+
+    private void AddDescendants(Transform parent, List<GameObject> list)
+    {
+        foreach (Transform child in parent)
+        {
+            list.Add(child.gameObject);
+            AddDescendants(child, list);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Check for mouse click outside the panel
+        if (Input.GetMouseButtonDown(0) && !IsPointerOverPanel())
+        {
+            // If the panel is active, close it
+            if (Parent.activeSelf)
+            {
+                GameObject.Find("controller/PlayerCapsule").GetComponent<FirstPersonController>().Unpause();
+                Parent.SetActive(false);
+
+            }
+        }
     }
+
+    // Check if the mouse is over the specified panel
+    private bool IsPointerOverPanel()
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = Input.mousePosition;
+
+        // Raycast to determine if the pointer is over a UI object
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+
+        // Check if the UI object under the pointer is the specified panel
+        return results.Count > 0 && children.Contains(results[0].gameObject);
+    }
+
+
 
     private void OnEnable() {
         this.transform.Find("Question").gameObject.SetActive(false);
--- Assets/Scripts/UI/TextPanel.cs
+++ Assets/Scripts/UI/TextPanel.cs
@@ -3,23 +3,66 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using StarterAssets;
 
 public class TextPanel : MonoBehaviour
 {
     private GameObject TextTitle;
     private GameObject TextDescription;
+    private GameObject Parent;
+    private List<GameObject> children;
 
     // Start is called before the first frame update
     void Start()
     {
         TextTitle = transform.Find("TextTitle").gameObject;
         TextDescription = transform.Find("Text").gameObject;
+        Parent = transform.parent.gameObject;
+
+        children = new List<GameObject>();
+        AddDescendants(transform, children);
+        children.Add(gameObject);
+
+    }
+
+    private void AddDescendants(Transform parent, List<GameObject> list)
+    {
+        foreach (Transform child in parent)
+        {
+            list.Add(child.gameObject);
+            AddDescendants(child, list);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Check for mouse click outside the panel
+        if (Input.GetMouseButtonDown(0) && !IsPointerOverPanel())
+        {
+            // If the panel is active, close it
+            if (Parent.activeSelf)
+            {
+                GameObject.Find("controller/PlayerCapsule").GetComponent<FirstPersonController>().Unpause();
+                Parent.SetActive(false);
+
+            }
+        }
+    }
+
+    // Check if the mouse is over the specified panel
+    private bool IsPointerOverPanel()
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = Input.mousePosition;
+
+        // Raycast to determine if the pointer is over a UI object
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+
+        // Check if the UI object under the pointer is the specified panel
+        return results.Count > 0 && children.Contains(results[0].gameObject);
     }
 
     public void setText(string Title, string Body){
