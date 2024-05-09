fbb0851054fd1b58102acbd780de59f50c1c3325
Jeremiah <jlee814@calpoly.edu>
2024-05-08 23:07:24 -0700

    Boundary Effect Work

--- Assets/boundaryTrigger.cs
+++ Assets/boundaryTrigger.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 public class boundaryTrigger : MonoBehaviour
 {
     public GameObject objectToDissolve;
-
+    private Coroutine boundaryCoroutine;
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Collided with Trigger");
-        StartCoroutine(objectToDissolve.GetComponent<dissolver>().ShowBoundary());
+        boundaryCoroutine = StartCoroutine(objectToDissolve.GetComponent<GridController>().ShowBoundary());
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("Exited Trigger");
+        StopCoroutine(boundaryCoroutine);
     }
 }
