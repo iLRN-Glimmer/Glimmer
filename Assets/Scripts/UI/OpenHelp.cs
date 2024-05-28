1f1ecfbcea0096feda1450af25902389f58c757e
Angela Chen <achen313@calpoly.edu>
2024-05-27 19:49:28 -0700

    update tutorial window

--- Assets/Scripts/UI/OpenHelp.cs
+++ Assets/Scripts/UI/OpenHelp.cs
@@ -1,9 +1,13 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 using StarterAssets;
+using System.Linq;
+using System.Collections.Generic;
 
 public class OpenHelp : MonoBehaviour
 {
-    public GameObject panel; // Reference to the panel you want to activate/deactivate
+    [SerializeField]
+    private GameObject panel; // Reference to the panel you want to activate/deactivate
     private bool freezed;
 
     void Start()
@@ -18,28 +22,28 @@ public class OpenHelp : MonoBehaviour
 
     void Update()
     {
-        // Check if the "M" key is pressed
+        // Check if the "Tab" key is pressed
         if (Input.GetKeyDown(KeyCode.Tab))
         {
             Debug.Log("Tab key pressed!");
 
-        
-            if (panel != null) {
+            if (panel != null)
+            {
                 // If the panel is currently inactive, activate it
                 //find freeze state
                 freezed = GameObject.Find("controller/PlayerCapsule").GetComponent<FirstPersonController>().GetFreeze();
-                
+
                 // Toggle the active state of the panel
                 if (!panel.activeSelf)
                 {
-                    if(!freezed) {
+                    if (!freezed)
+                    {
                         panel.SetActive(true);
 
                         // Ensure cursor is visible and unlocked
                         Cursor.lockState = CursorLockMode.None;
                         Cursor.visible = true;
                         GameObject.Find("controller/PlayerCapsule").GetComponent<FirstPersonController>().SetFreeze();
-                        
                     }
                 }
                 else // If the panel is currently active, deactivate it
@@ -49,6 +53,30 @@ public class OpenHelp : MonoBehaviour
                 }
             }
         }
-    
-}
+
+        // Check for mouse click outside the panel
+        if (Input.GetMouseButtonDown(0) && !IsPointerOverPanel())
+        {
+            // Close the panel
+            if (panel != null && panel.activeSelf)
+            {
+                GameObject.Find("controller/PlayerCapsule").GetComponent<FirstPersonController>().Unpause();
+                panel.GetComponent<DialogBox>().CloseDialog();
+            }
+        }
+    }
+
+    // Check if the mouse is over the specified panel
+    private bool IsPointerOverPanel()
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = Input.mousePosition;
+
+        // Raycast to determine if the pointer is over a UI object
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+
+        // Check if the UI object under the pointer is a child of the panel's parent GameObject
+        return results.Any(result => result.gameObject.transform.IsChildOf(panel.transform));
+    }
 }
